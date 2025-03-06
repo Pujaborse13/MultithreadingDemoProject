61c99e3f61e32359a364880d5203faf02806aab6
Puja Borse <pujaborse13@gmail.com>
2025-03-06 17:07:03 +0530

    Sysnchronous Demo Project

---
+++ SynchronousDemo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MutithreadingProject
+{
+    public class SynchronousDemo
+    {
+        public void MyTask()
+        {
+            Console.WriteLine("Task 1 start....");
+            Task1();
+
+            Console.WriteLine("Task 2 start....");
+            Task2();
+
+            Console.WriteLine("Task 3 start...");
+            Task3();
+        }
+
+
+
+        public void Task1()
+        { 
+            Thread.Sleep(4000);
+            Console.WriteLine("Task 1 Completed");
+
+        }
+
+
+        public void Task2()
+        {
+            Thread.Sleep(2000);
+            Console.WriteLine("Task 2 Completed");
+            
+        
+        }
+
+        public void Task3()
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine("Task 3 Completed");
+
+
+        }
+
+
+
+    }
+}
