5abb1494d7f0d6c2333f9fd290c09b087f2c44d5
Puja Borse <pujaborse13@gmail.com>
2025-03-06 17:03:47 +0530

    Program Class Added

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using MutithreadingProject;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        SynchronousDemo ex = new SynchronousDemo();
+        ex.MyTask();
+
+    }
+
+
+
+    //static async Task Main()
+    //{
+    //    AsynchronousDemo demo = new AsynchronousDemo();
+    //    await demo.MyTask();
+    //}
+
+
+
+}
