42fbd5d250d92052dbb27a0d40774039fb5f183a
Puja Borse <pujaborse13@gmail.com>
2025-03-07 16:15:33 +0530

    Added MultithreadingDemo.cs to MultithreadingDemo branch

---
+++ MultithreadingDemo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MutithreadingProject
+{
+    public class MultithreadingDemo
+    {
+
+       public async Task AllTask()
+        {
+            Thread thread1 = new Thread(PrintNumbers);
+            thread1.Start();
+
+            Thread thread2 = new Thread(PrintLetters);
+            thread2.Start();
+
+
+
+        }
+
+        public static void PrintNumbers()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} :  {i}");
+                Thread.Sleep(2000);
+
+            }
+
+        }
+
+
+        static void PrintLetters()
+        {
+            for (char c = 'A'; c <= 'E'; c++)
+            {
+                Console.WriteLine(c);
+                Thread.Sleep(2000);
+            }
+        }
+
+
+
+    }
+}
